f4c4e1099bc0d9c028d2fe6ef77473efc5e2785f
Ximer <20438890+Ximer612@users.noreply.github.com>
2023-11-30 16:26:02 +0100

    Game source

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Assembly RayLib game
  An assembly game I made using the raylib library.
+
+To build the game do -> python game.py game.bin game.exe
---
+++ game.py
@@ -0,0 +1,40 @@
+from pelinker import COFF, Executable
+import sys
+
+with open(sys.argv[1], "rb") as handle:
+    coff = COFF(handle.read())
+
+exe = Executable()
+
+for (
+    section_name,
+    section_permissions,
+    section_data,
+    relocations
+) in coff.sections:
+    new_section = exe.add_section(section_name, section_permissions, section_data)
+    if section_name == ".text":
+        exe.entry_point = new_section.rva
+    for reloc_name, reloc_offset, reloc_type in relocations:
+        new_section.add_relocation_symbol(reloc_name, reloc_offset, reloc_type)
+
+exe.import_symbols("user32.dll", ["MessageBeep"])
+exe.import_symbols("kernel32.dll", ["ExitProcess"])
+exe.import_symbols(
+    "raylib.dll",
+    [
+        "InitWindow",
+        "SetTargetFPS",
+        "WindowShouldClose",
+        "BeginDrawing",
+        "ClearBackground",
+        "DrawText",
+        "EndDrawing",
+        "CloseWindow",
+        "DrawRectangle",
+        "IsKeyDown",
+    ],
+)
+
+with open(sys.argv[2], "wb") as handle:
+    handle.write(exe.link())
